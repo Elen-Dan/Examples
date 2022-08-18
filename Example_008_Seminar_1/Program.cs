a25a875d79abe57ec30a0aae29fb78fccf7ea9a2
El_Dan <lilit-li@ya.ru>
2022-08-18 22:53:55 +0400

    Умножени числа на себя

---
+++ Example_008_Seminar_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Создаем переменную 
+double a;
+string? input;
+double result;
+// Вводим число
+Console.WriteLine("Введите число: ");
+input = Console.ReadLine();
+// C# convert string to int
+a = double.Parse(input);
+// Возводим в квадрат
+result = a * a;
+// Вывод результата
+Console.WriteLine("Результат: " + result);
+Console.WriteLine(a * a);
+
